1941a245d511948afbe5c21c3ad900ec0a6951df
chandra <sekharchandra936@gmail.com>
2022-02-28 15:00:32 -0500

    added vue script

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,11 +1,16 @@
 <script setup lang="ts">
-import Nav from './components/nav.vue';
+import Nav from './components/Nav.vue';
 import Messagess from './pages/Messagess.vue';
 
 </script>
 
 <template>
      <Nav />
+     <div class="container">
+          <section class="section">
+               <h1 class="title">Marachu Chandra Sekhar</h1>
+          </section>
+     </div>
      <Messagess />
      </template>
      <style>
--- client/src/pages/Messagess.vue
+++ client/src/pages/Messagess.vue
@@ -1,4 +1,30 @@
 <script setup lang="ts">
+import { ref, reactive, onMounted } from "vue";
+  
+            const message = ref( 'Hello Vue!');
+            const currentTab = ref ( 'All');
+            const prompt = ref('Waiting for input');
+            const notifications = reactive([
+              { type: 'primary' , message: 'this is a primary notification'},
+              { type: 'link' , message: 'link notification'},
+              { type: 'success' , message: 'its done'},
+              { type: 'warning' , message: 'warning'},
+              { type: 'danger' , message: 'you have not done it'},
+            ] );
+          
+            function cardClick(){
+               message.value = "you clicked the card";
+             }
+            function close(index: number){
+               notifications.splice(index, 1);
+             }
+          
+          
+          onMounted(() =>{
+            setInterval(() =>{
+              prompt.value +='.';
+            }, 500);
+          });
 
 </script>
 
