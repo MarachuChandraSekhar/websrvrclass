fcd2394fdbd93ca4003b7bdc7d8fb1f5f00dce0a
chandra <sekharchandra936@gmail.com>
2022-04-28 14:05:13 -0400

    changes

--- client/src/models/user.ts
+++ client/src/models/user.ts
@@ -6,33 +6,5 @@ export interface User {
     email: string;
     pic: string;
     id: number;
+    token?: string;
 }
-export const list: User[]= [
-    {
-        firstName: 'Chandra Sekhar',
-        lastName: 'Marachu',
-        handle: 'ChandraSekharMarachu',
-        password: 'mcs',
-        email: 'chandra@cs.com',
-        pic: 'https://randomuser.me/api/potraits/men/3.jpg',
-        id: 1,
-    },
-    {
-        firstName: 'Chandra ',
-        lastName: 'Marachu',
-        handle: 'ChandraMarachu',
-        password: 'mc',
-        email: 'chandra@c.com',
-        pic: 'https://randomuser.me/api/potraits/men/5.jpg',
-        id: 1,
-    },
-    {
-        firstName: 'Sekhar',
-        lastName: 'Marachu',
-        handle: 'SekharMarachu',
-        password: 'ms',
-        email: 'Sekhar@s.com',
-        pic: 'https://randomuser.me/api/potraits/men/7.jpg',
-        id: 1,
-    },
-];
--- client/src/router/index.ts
+++ client/src/router/index.ts
@@ -6,26 +6,20 @@ import Generic from '../pages/Generic.vue';
 import Login from '../pages/Login.vue';
 import { useSession } from "../models/session";
 
-// 2. Define some routes
-// Each route should map to a component.
-// We'll talk about nested routes later.
 const routes: RouteRecordRaw[] = [
   { path: '/', component: Home },
   { path: '/about', component: Generic, props: { title: 'About Page!' } },
   { path: '/contact', component: Generic, props: { title: 'Contact Page!' } },
   { path: '/login', component: Login },
   { path: '/signup', component: Generic, props: { title: 'Signup Page!' } },
-  { path: '/wall', component: () => import('../pages/Wall.vue') },
+  { path: '/wall/:handle?', component: () => import('../pages/Wall.vue') },
   { path: '/hidden', component: Generic, props: { title: 'You reached the hidden Page!' } },
 ]
 
-// 3. Create the router instance and pass the `routes` option
-// You can pass in additional options here, but let's
-// keep it simple for now.
 const router = createRouter({
-  // 4. Provide the history implementation to use. We are using the hash history for simplicity here.
+  
   history: createWebHistory(),
-  routes, // short for `routes: routes`
+  routes, 
   linkActiveClass: 'is-active',
 })
 
@@ -39,7 +33,7 @@ router.beforeEach((to, from) => {
     const protectedUrls = ['/messages', '/wall', '/feed', '/hidden'];
     console.log({ protectedUrls });
 
-    if (protectedUrls.includes(to.path)) { // list of paths that require login
+    if (protectedUrls.includes(to.path.toLowerCase())) { 
         console.log('requires login');
         if (!session.user) {
             return '/login';
